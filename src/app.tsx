c53a420f99dfd6e973915bc7237d4fbb5056ddef
Bambang Adi Prakoso <bambang.prakoso@agriaku.com>
2025-11-23 18:31:20 +0700

    Add tanstack query

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
